952f703e59455e544358fde090f32e51e26f4280
acolocho772 <amoscolocho5@gmail.com>
2025-11-18 20:41:47 +0000

    Added insertion sort and merge sort

---
+++ src/InsertionSort.cc
@@ -0,0 +1,19 @@
+//Amos Colocho
+
+//Insertion Sort
+#include<iostream>
+//long variables to deal with larger data sets
+void insertionSort(long arr[],long n) {
+    for (long i =1; i< n; ++i) {
+        long g=arr[i];
+        long j = i -1;
+        while (j >= 0 && arr[j] >g) {
+            arr[j +1] = arr[j];
+            j = j-1;
+        }
+        arr[j +1]= g;
+    }
+}
+
+
+
---
+++ src/MergeSort.cc
@@ -0,0 +1,47 @@
+//Amos Colocho
+//Merge function first
+void merge(long arr[], long l, long m, long r) {
+    long n1 = m-l + 1;
+    long n2 = r -m;
+    long L[n1], R[n2];
+
+    for (long i = 0;i < n1; i++)
+        L[i] =arr[l+i];
+    for (long j =0;j<n2;j++)
+        R[j]= arr[m + 1 + j];
+
+    long i = 0, j=0,k =l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k]=L[i];
+            i++;
+        } else {
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
+}
+//Other function calling the one above (This is the actual mergesort)
+void mergeSort(long arr[], long l, long r) {
+    if (l >= r) return;
+
+    long m = l+(r-l)/ 2;
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+}
+
+
+
+
